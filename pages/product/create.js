2a4d8d25600475af7c51e2e3686587a170ba26fc
dickyrex19 <enjoybeat19@gmail.com>
2022-02-20 22:16:04 +0700

    modify create product

--- pages/product/create.js
+++ pages/product/create.js
@@ -2,15 +2,31 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 
-export default function create(props) {
-  const productType = props.data.data;
-
+export default function create() {
   const [productName, setProductName] = useState("");
-  const [productTypeId, setProductTypeId] = useState();
+  const [productTypeId, setProductTypeId] = useState(0);
   const [price, setPrice] = useState(0);
   const [qty, setQty] = useState(0);
   const [imageUrl, setImageUrl] = useState("");
   const [desc, setDesc] = useState("");
+
+  const [productType, setproductType] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+
+    axios
+      .get("http://18.140.1.124:8081/product/type", config)
+      .then(({ data }) => {
+        setproductType(data.data);
+      })
+      .catch((err) => {
+        console.log(err, "error bang");
+      });
+  }, []);
 
   function handleAddProduct() {
     const body = {
@@ -32,9 +48,7 @@ export default function create(props) {
         </div>
         <form>
           <div className="mb-3">
-            <label for="inputProduct" className="form-label fw-bold">
-              Product Name
-            </label>
+            <label className="form-label fw-bold">Product Name</label>
             <input
               type="text"
               className="form-control"
@@ -45,9 +59,7 @@ export default function create(props) {
             />
           </div>
           <div className="mb-3">
-            <label for="inputProductType" className="form-label fw-bold">
-              Product Type
-            </label>
+            <label className="form-label fw-bold">Product Type</label>
             <select
               className="form-select"
               aria-label="Default select example"
@@ -55,7 +67,6 @@ export default function create(props) {
                 setProductTypeId(e.target.value);
               }}
             >
-              <option selected>Choose Product Type</option>
               {productType.map((product, i) => (
                 <option key={i} value={product.id}>
                   {product.name}
@@ -65,9 +76,7 @@ export default function create(props) {
           </div>
           <div className="row">
             <div className="mb-3 col-md-6">
-              <label for="inputPrice" className="form-label fw-bold">
-                Price
-              </label>
+              <label className="form-label fw-bold">Price</label>
               <input
                 type="number"
                 className="form-control"
@@ -78,9 +87,7 @@ export default function create(props) {
               />
             </div>
             <div className="mb-3 col-md-6">
-              <label for="inputQty" className="form-label fw-bold">
-                Quantity
-              </label>
+              <label className="form-label fw-bold">Quantity</label>
               <input
                 type="number"
                 className="form-control"
@@ -92,9 +99,7 @@ export default function create(props) {
             </div>
           </div>
           <div className="mb-3">
-            <label for="inputProduct" className="form-label fw-bold">
-              Image URL
-            </label>
+            <label className="form-label fw-bold">Image URL</label>
             <input
               type="text"
               className="form-control"
@@ -105,9 +110,7 @@ export default function create(props) {
             />
           </div>
           <div className="mb-3">
-            <label for="inputDesc" className="form-label fw-bold">
-              Description
-            </label>
+            <label className="form-label fw-bold">Description</label>
             <textarea
               name="description"
               className="form-control"
@@ -129,26 +132,4 @@ export default function create(props) {
       </Container>
     </>
   );
-}
-
-export async function getServerSideProps() {
-  // const token = localStorage.getItem("token");
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjp0cnVlLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImV4cCI6MTY0NTM5NDgxOCwiaWQiOjIsInBhc3N3b3JkIjoiYWRtaW4ifQ.zcHp6iIeHyufdZmqZayWMcL_lsHDv8uAVY1Q1PjBTV4";
-
-  const config = {
-    headers: { Authorization: `Bearer ${token}` },
-  };
-
-  const result = await axios.get(
-    "http://18.140.1.124:8081/product/type",
-    config
-  );
-  const data = await result.data;
-
-  return {
-    props: {
-      data,
-    },
-  };
 }
